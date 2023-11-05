2e7df6bbd162fa85275211ace4810a117e5ee235
dinesh nagpal <133826705+sabby74@users.noreply.github.com>
2023-11-05 02:39:52 -0500

    updated DEBUG variable in settings.py

--- django_shops/settings.py
+++ django_shops/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True if os.environ['MODE'] == 'dev' else False
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
